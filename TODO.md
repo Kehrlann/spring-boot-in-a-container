319e5b2ffad777a84554a332d555d2c8ad7dfb4a
Daniel Garnier-Moiroux <git@garnier.wf>
2019-06-10 18:33:19 +0200

    Add cloud native buildpacks

--- TODO.md
+++ TODO.md
@@ -7,7 +7,7 @@
 [x] Openjdk-alpine
 [x] Distroless
 [x] Organize layers
-[ ] Jib
+[x] Jib
 
 ## Potentially
 
@@ -15,4 +15,6 @@
   - without caching, takes about 5 to 10 minutes, depending on the network speed
 [x] Multi-stage build, with cache
   - With caching (but doing a "clean package" rather than just "install"), takes 10 to 15 seconds.
-[ ] Cloud native buildpacks
+[x] Cloud native buildpacks
+  - First start is very long: get the builder image, get Java, build (without dependencies)
+  - Second run about 30s, both heroku and CF CN
--- build-all-images.sh
+++ build-all-images.sh
@@ -14,3 +14,9 @@ docker build -t kehrlann/pet-clinic:distroless -f dockerfiles/4_distroless.docke
 source build-layers.sh
 docker build -t kehrlann/pet-clinic:multistage -f dockerfiles/6_multistage.dockerfile .
 DOCKER_BUILDKIT=1 docker build -t kehrlann/pet-clinic:multistage-cache -f dockerfiles/7_multistage-cache.dockerfile .
+
+# cloud native images
+pushd spring-petclinic
+pack build kehrlann/pet-clinic:pack-cf-cn-buildpack --builder=cloudfoundry/cnb
+pack build kehrlann/pet-clinic:pack-heroku-buildpack --builder=heroku/buildpacks
+popd
