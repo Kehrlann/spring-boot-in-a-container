9e3eea03c64c7835e6f15406e52f3ffd7440d1b8
Daniel Garnier-Moiroux <git@garnier.wf>
2019-06-10 14:21:18 +0200

    build java project before building the images

--- build-all-images.sh
+++ build-all-images.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Start by building the app
+pushd spring-petclinic
+./mvnw clean package
+popd
+
+# Build the images
+## Basic images
 docker build -t kehrlann/pet-clinic:ubuntu -f dockerfiles/1_ubuntu.dockerfile .
 docker build -t kehrlann/pet-clinic:openjdk-8 -f dockerfiles/2_openjdk.dockerfile .
 docker build -t kehrlann/pet-clinic:openjdk-8-alpine -f dockerfiles/3_openjdk-alpine.dockerfile .
