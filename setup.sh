0fab843d0942d8b6d5e03928880b40ffc41e2e5f
Daniel Garnier-Moiroux <git@garnier.wf>
2019-06-24 13:01:25 +0200

    Update setup scripts: setup reveal.js

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/spring-projects/spring-petclinic.git --depth=1
+git submodule update --init
+rm reveal.js/index.html
+ln -s $PWD/presentation.html reveal.js/index.html
+ln -s $PWD/images reveal.js/
