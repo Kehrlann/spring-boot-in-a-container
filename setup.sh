ceb1b24aa59b3fe56b6ccc4a265a39333319fbf7
Daniel Garnier-Moiroux <git@garnier.wf>
2019-12-11 10:29:49 +0100

    Make spring-petclinic a submodule

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/spring-projects/spring-petclinic.git --depth=1
 git submodule update --init
 rm reveal.js/index.html
 ln -s $PWD/presentation.html reveal.js/index.html
