4587d8f86981494f2f1c7e5dd1ffcd76ac2f1e5a
Daniel Garnier-Moiroux <git@garnier.wf>
2019-12-17 10:54:44 +0100

    Automatically add jib plug in Spring petclinic pom.xml on setup

--- setup.sh
+++ setup.sh
@@ -7,3 +7,12 @@ ln -s $PWD/images reveal.js/
 pushd reveal.js
 npm i
 popd
+
+pom="spring-petclinic/pom.xml"
+if ! grep 'jib-maven-plugin' "$pom" > /dev/null; then
+  echo ' ⚙️ Adding jib to pom.xml...'
+  gsed -i '/<\/plugins>/e cat jib.xml' "$pom"
+  echo ' ✅ Done !'
+else
+  echo ' ✅ Jib already in pom.xml, ignoring.'
+fi
